8ebad8ff38eb0ea98db01ece01605de390259f6d
Rhamses <rhamses@iMac-de-Rhamses.local>
2022-07-16 15:58:10 -0300

    3edd6c8

---
+++ _nuxt/7055c34.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{252:function(t,n,e){"use strict";e.r(n);var c=e(42),component=Object(c.a)({},(function(){this._self._c;return this._m(0)}),[function(){var t=this._self._c;return t("div",[t("p",[this._v("oteste")])])}],!1,null,null,null);n.default=component.exports}}]);
