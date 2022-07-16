87664985f99b4946a5f71881db8c64d2d80604b4
Rhamses <rhamses@iMac-de-Rhamses.local>
2022-07-16 15:42:10 -0300

    update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/nuxt-github-io/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
