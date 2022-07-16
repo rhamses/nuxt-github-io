dda4e14dde37192d8e0a182b3f39f0a2ac2c5c59
Rhamses <rhamses@iMac-de-Rhamses.local>
2022-07-16 16:06:43 -0300

    fix static

--- _nuxt/39cce1d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{253:function(n,t,e){"use strict";e.r(t);var c=e(42),component=Object(c.a)({},(function(){this._self._c;return this._m(0)}),[function(){var n=this._self._c;return n("div",[n("p",[this._v("asdd")])])}],!1,null,null,null);t.default=component.exports}}]);
